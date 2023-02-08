9558506c4fdf752633a54cee99fbbd6c8560d085
gueckmooh <gueckmooh@pm.me>
2023-02-08 15:47:29 +0100

    Some minor updates

--- scripts/installer/package.py
+++ scripts/installer/package.py
@@ -6,8 +6,20 @@ from .meson import get_meson, Meson, MesonError
 
 from .source import Source
 
+
+class PackageForbidden(Exception):
+    """The error that is raised if a forbidden action if performed on the
+    package."""
+
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
+
+
 class Package(ABC):
+    """Abstract class that represents a package."""
+
     name: str
+
     def __init__(self, name: str) -> None:
         self.name = name
 
@@ -36,8 +48,11 @@ class Package(ABC):
     #         self.build(path)
     #     self.install(path)
 
+
 class MesonPackage(Package):
+
     __src: Source
+
     def __init__(self, name: str, src: Source) -> None:
         super().__init__(name)
         self.__src = src
@@ -60,6 +75,7 @@ class MesonPackage(Package):
     def is_installable(self) -> bool:
         return True
 
+
 class NonePackage(Package):
     def __init__(self, name: str) -> None:
         super().__init__(name)
@@ -67,5 +83,8 @@ class NonePackage(Package):
     def is_installable(self) -> bool:
         return False
 
+    def build(self, path: Path) -> None:
+        raise PackageForbidden(f'Package {self.name} cannot be built')
+
     def checkout(self, path: Path) -> None:
-        return
+        raise PackageForbidden(f'Package {self.name} cannot be checked out')
